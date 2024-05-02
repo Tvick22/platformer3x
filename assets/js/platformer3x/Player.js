39dec69a65a92a671efe40ed0d2351a6da1a7691
TravisCallow <travisgcallow@gmail.com>
2024-05-02 13:56:52 -0700

    added skibidi toilet flush sound

--- assets/js/platformer3x/Player.js
+++ assets/js/platformer3x/Player.js
@@ -234,7 +234,7 @@ export class Player extends Character {
         // Tube collision check
         if (this.collisionData.touchPoints.other.id === "tube" 
             || this.collisionData.touchPoints.other.id === "tree"
-            || this.collisionData.touchPoints.other.id === "cabin") {
+            || this.collisionData.touchPoints.other.id === "cabin" || this.collisionData.touchPoints.other.id === "toiletEnd") {
 
             // Collision with the left side of the Tube
             if (this.collisionData.touchPoints.other.left) {
